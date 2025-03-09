56f387eeb878c77caca53d79c260575c1aba9c43
Ashwin K Whitchurch <ashwin@protocentral.com>
2025-03-09 11:37:05 +0530

    Add iOS config

--- move/macos/Flutter/GeneratedPluginRegistrant.swift
+++ move/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_picker
 import file_saver
+import flutter_blue_plus_darwin
 import open_file_mac
 import package_info_plus
 import path_provider_foundation
@@ -16,6 +17,7 @@ import shared_preferences_foundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
