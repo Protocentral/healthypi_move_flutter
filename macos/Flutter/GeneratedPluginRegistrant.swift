99df7127bce5f0734445f22e3208ed712315cd83
Ashwin K Whitchurch <ashwin@protocentral.com>
2026-07-19 20:34:08 +0530

    build: regenerate macOS/Windows plugin registrants after dep cleanup
    flutter pub get dropped the removed plugins (device_info_plus, geolocator, …)
    from the generated registrants. Commit the regenerated files so the tree matches
    the pruned pubspec.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_picker
 import flutter_archive
-import geolocator_apple
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -18,10 +16,8 @@ import universal_ble
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
