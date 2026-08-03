d071a81694cfefa04978303462eb76dad30ba432
suvarna84 <suvarna@circuitects.com>
2026-08-03 15:06:35 +0530

    updated gradle config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,7 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -20,7 +20,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
 
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
